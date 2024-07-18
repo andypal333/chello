e0a6b2f631ff68b7fc185cd6e49e0aaf6746c84a
Andy Pal <andypal333@gmail.com>
2024-07-18 08:10:46 -0400

    Enhance code comments.

--- main.c
+++ main.c
@@ -1,15 +1,18 @@
-// Simple C program to display "Hello World"
- 
-// Header file for input output functions
+// A simple C program to display the message "Hello World"
+
+// Include the standard input/output header file
+// This header file is necessary for using the printf function
 #include <stdio.h>
- 
-// main function -
-// where the execution of program begins
+
+// The main function serves as the entry point of the program
+// Program execution starts from the main function
 int main()
 {
- 
-    // prints hello world
+    // Use the printf function to output the message "Hello World" to the console
+    // printf is a standard library function defined in <stdio.h>
     printf("Hello World");
- 
+
+    // Return 0 to indicate successful completion of the program
+    // The return value of main is used as the exit status of the program
     return 0;
 }
